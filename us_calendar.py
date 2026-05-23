7cb1b62e855295020bc01314d36c5cd7bbee47f7
P2SAMAPA <pranabpattanaik@gmail.com>
2026-05-24 07:48:14 +0800

    Create us_calendar.py

---
+++ us_calendar.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+import pandas as pd
+from pandas.tseries.offsets import CustomBusinessDay
+from pandas.tseries.holiday import USFederalHolidayCalendar
+
+cal = USFederalHolidayCalendar()
+us_bd = CustomBusinessDay(calendar=cal)
+
+def next_trading_day(date=None):
+    if date is None:
+        date = datetime.today()
+    next_day = date + us_bd
+    return next_day.strftime("%Y-%m-%d")
